bb4e06e8e9c6aa001a25709327e644ca00595909
atimmer <atimmer@terpmail.umd.edu>
2026-03-07 13:55:36 -0500

    removed metricsConfig

--- backend/src/main/java/com/fredvested/web/WaitlistApplication.java
+++ backend/src/main/java/com/fredvested/web/WaitlistApplication.java
@@ -2,18 +2,10 @@ package com.fredvested.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
-import org.springframework.boot.actuate.autoconfigure.metrics.web.tomcat.TomcatMetricsAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-    SystemMetricsAutoConfiguration.class,
-    TomcatMetricsAutoConfiguration.class
-})
+@SpringBootApplication
 public class WaitlistApplication {
     public static void main(String[] args) {
-        // Disable JMX and container detection to prevent cgroup access
-        System.setProperty("com.sun.management.jmxremote", "false");
-        System.setProperty("java.awt.headless", "true");
         SpringApplication.run(WaitlistApplication.class, args);
     }
 }  
